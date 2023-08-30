676529e20430437d2d18a133571c04e34d850d4d
Crowdbotics <engineering@crowdbotics.com>
2023-08-30 23:19:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-water-dev-113016",
+  url: "https://tight-water-dev-113016.botics.co",
+  api: "https://tight-water-dev-113016.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
